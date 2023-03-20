36167be4c0d0ebe579cf61559021b219d8587ff8
powmks <mihalichklimov@gmail.com>
2023-03-20 22:18:55 +0200

    added Models

---
+++ WatermarkApi/Models/SourceImage.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace WatermarkApi.Models
+{
+    public class SourceImage
+    {
+        public int Id { get; set; }
+        public Bitmap Image { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+}
