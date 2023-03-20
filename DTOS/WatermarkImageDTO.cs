7c9f243ebdea5d1f3064a6a4c4d547a44d8bb476
powmks <mihalichklimov@gmail.com>
2023-03-20 22:24:27 +0200

    added DTOs

---
+++ DTOS/WatermarkImageDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOS
+{
+    internal class WatermarkImageDTO
+    {
+        public int Id { get; set; }
+        public Bitmap Image {  get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
+}
